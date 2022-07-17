ea8401b78998b94e0cbd27252ca14efd5fc58789
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2022-07-17 23:15:54 +0530

    Update _inlinebot.py

--- DarkWeb/plugins/_inlinebot.py
+++ DarkWeb/plugins/_inlinebot.py
@@ -70,7 +70,7 @@ def button(page, modules):
     return [max_pages, buttons]
 
 
-if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
+if Var.BOT_USERNAME is not None and tgbot is not None:
 
     @tgbot.on(InlineQuery)  # pylint:disable=E0602
     async def inline_handler(event):
