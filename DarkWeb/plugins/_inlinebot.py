6bcba188daa434f0b607b9b97529e5a7a768b566
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2022-07-18 11:30:37 +0530

    Update _inlinebot.py

--- DarkWeb/plugins/_inlinebot.py
+++ DarkWeb/plugins/_inlinebot.py
@@ -71,6 +71,7 @@ if Var.BOT_USERNAME is not None and tgbot is not None:
             rev_text = query[::-1]
             veriler = button(0, sorted(CMD_HELP))
             result = await builder.article(
+            f"Hey! Only use .help please",
                 text=f"**༆ {mention} ༆ \n⭐ 𝚃𝚘𝚝𝚊𝚕 𝙼𝚘𝚍𝚞𝚕𝚎𝚜 𝙸𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍⭆:`{len(CMD_HELP)}`\n**📖 Pαցҽ⭆:** 1/{veriler[0]}",
             #    file=_file_to_replace,
                 buttons=veriler[1],
