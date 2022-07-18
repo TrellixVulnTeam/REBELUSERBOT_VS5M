bafa14962363ac3af2ae6ed0bec902736411a6cc
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2022-07-18 11:17:31 +0530

    Update _inlinebot.py

--- DarkWeb/plugins/_inlinebot.py
+++ DarkWeb/plugins/_inlinebot.py
@@ -77,7 +77,7 @@ if Var.BOT_USERNAME is not None and tgbot is not None:
         builder = event.builder
         result = None
         query = event.text
-        if event.query.user_id == bot.uid and query == "@REBELBOT_SUPPORT":
+        if event.query.user_id == bot.uid and query == "darkweb_help":
             rev_text = query[::-1]
             veriler = button(0, sorted(CMD_HELP))
             result = await builder.photo(
