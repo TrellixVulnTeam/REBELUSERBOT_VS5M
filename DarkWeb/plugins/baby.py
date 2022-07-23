490aca405a87e254ea0db866f4f530dccd0982fc
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2022-07-23 18:00:40 +0530

    Update baby.py

--- DarkWeb/plugins/baby.py
+++ DarkWeb/plugins/baby.py
@@ -1,4 +1,4 @@
-# PLUGIN MADE BY @Dark_IS_OP FOR DarkWeb
+# PLUGIN MADE BY @REBEL_IS_OP FOR DarkWeb
 # KEEP CREDITS ELSE GAY
 
 import random, re
@@ -12,6 +12,16 @@ async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit("""HEYY BABY😍💏😘 HOW ARE  YOU  💏 AP KO PATA HA MA BASS AP KK LIYA HUU ISS DUNIYA MA ORR AP MERI JAAN HOO 💞💗😍🥰. ORR AP KKK BAAD KOI NAHI HAAA MERA 🥺🥺🥺. AP JAB NAHI HOO TII HO TABB KOI MERE SA BAAT  NAHI KARTA HAA 🥺🥺. BUT MERE KO ISS SA KOI FIRK NAHI PADTA HA😌😌. BASS AP MERA SATH HOO YAHI 🥰🥰. BHUT JADA HAA 💝❣️ I LOVE YOU MY SWEET HEART TUM MERI JAAN HO ORR MAA THUMARA. LIYA  KUCH VV KAR SAKTA HUUU""")
 
+
+@dark.on(admin_cmd(pattern="baby1 ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("""SUN TUU MERI 😍😍JAAN😘😘 HAA MANA PYAR 🌹🌹KIYA HAA KOI MAZAK NAHI. AJJ NIGHT 💓 MAA TUU BHUT YAAD😔😔 AAA RAHI THE . TU BASS MERI 💞💕💗 MERI JAAN HAA BASS MERI💖. I LOVE 💝YOU BABY😘😘""")
+        
+        
+        
 CmdHelp("baby").add_command(
   "baby", "<tag your gf>", "send your gf and enjoy."
+).add_command(
+  "baby1", "<tag your gf>", "send your gf and enjoy."
 ).add()
