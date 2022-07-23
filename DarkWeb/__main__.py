307bec8b1c27ef11d3b4b700ba641b44faf1a48e
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2022-07-23 15:25:40 +0530

    Update __main__.py

--- DarkWeb/__main__.py
+++ DarkWeb/__main__.py
@@ -13,6 +13,7 @@ from pathlib import Path
 import asyncio
 import telethon.utils
 
+LOAD_ASSISTANT = os.environ.get("LOAD_ASSISTANT", True)
 os.system("pip install -U telethon")
 
 async def add_bot(bot_token):
@@ -51,14 +52,19 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
 
-import glob
-path = 'DarkWeb/assistant/*.py'
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
+if LOAD_ASSISTANT == True:
+    path = "DarkWeb/assistant/*.py"
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as f:
+            path1 = Path(f.name)
+            shortname = path1.stem
+            try:
+                start_assistant(shortname.replace(".py", ""))
+            except Exception as er:
+                print(er)
+else:
+    print("Assitant is Not Loading As U Have Disabled")
 
 import DarkWeb._core
 
